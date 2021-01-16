84e089426f9382b45805e36060d6999b2f79d075
keremaslan21 <77104545+keremaslan21@users.noreply.github.com>
2021-01-16 20:28:01 +0300

    Update eğlence.js

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -4,7 +4,7 @@ module.exports = {
     const { MessageEmbed } = require('discord.js')
       const embed = new  MessageEmbed()
       .setTitle('Eğlence Komutları')
-      .addField('Eğlence Komutları ', ' +kansahibi | +kibir | +kıvılcım | +ıslakkum | +zamandayolculuk | **Not: Daha Tamamlanmadı** ')
+      .addField('Eğlence Komutları ', ' +kansahini Kan Sahibi Adlı Şarkıdan Bi Parça Gösterir\n +kibir Kibir Adlı Şarkıdan Bi Parça Gösterir\n +ıslakkum Islak Kum Adlı Şarkıdan Bi Parça Gösterir\n +kıvılcım Kıvılcım Adlı Şarkıdan Bi Parça Gösterir\n +tehlikeli Tehlikeli Adlı Şarkıdan Bi Parça Gösterir\n +zamandayolculuk Zamanda Yolculuk Adlı Şarkıdan Bi Parça Gösterir\n **Not: Daha Tamamlanmadı**')
       message.channel.send(embed)
     }
  }
