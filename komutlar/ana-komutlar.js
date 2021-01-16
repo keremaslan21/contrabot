231948aac392c77993f08801d5c3abb1ff49bac4
keremaslan21 <77104545+keremaslan21@users.noreply.github.com>
2021-01-16 20:09:35 +0300

    Update ana-komutlar.js

--- komutlar/ana-komutlar.js
+++ komutlar/ana-komutlar.js
@@ -4,7 +4,7 @@ module.exports = {
     const { MessageEmbed } = require('discord.js')
       const embed = new  MessageEmbed()
       .setTitle('Ana Komutlar')
-      .addField('Ana Komutlar', '+çal <şarkıismi> | +ayrıl | +gununşarkısı  | +sunucubilgi | +kullanıcıbilgi <kişi>  | ')
+      .addField('Ana Komutlar', '+çal <şarkıismi> Şarkı Çalar\n +ayrıl Bot Sesli Kanaldaysa Ayrılır\n +kullanıcıbilgi <kişi> Kişinin Kullanıcı Bilgilerini Gösterir\n +sunucubilgi Sunucunun Bilgilerini Gösteririr\n +destek Botun Destek Sunucusunu Gösterir\n +davet Botu Sunucunusa Ekleme Linkini Gösterir ')
       message.channel.send(embed)
     }
  }
