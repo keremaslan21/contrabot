2afcd0918c01429e5a22d4236e1fbef73b6c67a9
keremaslan21 <77104545+keremaslan21@users.noreply.github.com>
2021-01-16 19:45:25 +0300

    Update ayrıl.js

--- komutlar/ayrıl.js
+++ komutlar/ayrıl.js
@@ -1,7 +1,7 @@
 module.exports = {
 kod: "ayrıl",
 async run (client, message, args) {
- if (!message.member.voice.channel) return.message.channel.send('Kanka Sesli Kanalda dEğiLsiN');
+ if (!message.member.voice.channel) return message.channel.send('Kanka Sesli Kanalda dEğiLsiN');
  if (!message.guild.me.voice.channel) return message.channel.send('kAnkA sEsli Kanalda dEğİLİm')
  message.member.voice.leave()
  message.channel.send('Sesli Kanaldan Çıktım Brom')
