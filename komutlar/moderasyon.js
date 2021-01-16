440a3d402ad7f1a3e0941498a4a54dff08813ad6
keremaslan21 <77104545+keremaslan21@users.noreply.github.com>
2021-01-16 20:31:41 +0300

    Update moderasyon.js

--- komutlar/moderasyon.js
+++ komutlar/moderasyon.js
@@ -4,7 +4,7 @@ module.exports = {
     const { MessageEmbed } = require('discord.js')
       const embed = new  MessageEmbed()
       .setTitle('Moderasyon Komutları')
-      .addField('Moderasyon Komutları', ' +kanalsil <kanal> | +kick <kişi> | +ban <kişi> |')
+      .addField('Moderasyon Komutları', ' +kanalsil <kanal> Etiketlediğiniz Kanalı Siler\n +kick <kişi> Etiketlediğiniz Kişiyi Kickler\n +ban <kişi> Etiketlediğiniz Kişiyi Banlar')
       message.channel.send(embed)
     }
  }
