591384c4e608bb47ed1beb7dc363feffdac78b14
keremaslan21 <77104545+keremaslan21@users.noreply.github.com>
2021-01-17 12:32:34 +0300

    Update oy.js

--- komutlar/oy.js
+++ komutlar/oy.js
@@ -4,7 +4,7 @@ module.exports = {
     const { MessageEmbed } = require('discord.js')
       const embed = new  MessageEmbed()
       .setTitle('Bota Oy Verme Linki')
-      .addField('', '[Oy](https://top.gg/bot/792336223108333598/vote)')
+      .addField('Oy Vermek İçin Tıkla', '[Oy](https://top.gg/bot/792336223108333598/vote)')
       message.channel.send(embed)
     }
  }
