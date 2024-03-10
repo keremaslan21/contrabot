881822b105e9400be00b065d64733e5e0f01b26d
!hyper <77104545+keremaslan21@users.noreply.github.com>
2024-03-10 16:20:34 +0300

    Update kick.js

--- komutlar/kick.js
+++ komutlar/kick.js
@@ -6,21 +6,12 @@ module.exports = {
     const neden = args1.join(" ")
     const { MessageEmbed } = require ('discord.js')
     const user = message.mentions.users.first();
-    // If we have a user mentioned
     if (user) {
-      // Now we get the member from the user
       const member = message.guild.member(user);
-      // If the member is in the guild
       if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
         member
-          .kick('Optional reason that will display in the audit logs')
+          .kick('opsiyonel')
           .then(() => {
-            // We let the message author know we were able to kick the person
            const kanal = message.guild.channels.find(ch => ch.name === 'kick-log')
            const embed = new MessageEmbed()
            .setTitle('Herkes Arzularken Ben Siker Geçerim')
@@ -30,18 +21,12 @@ module.exports = {
            kanal.send(embed)
           })
           .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('Herkes Arzularken Ben Siker Geçerim :middle_finger: ');
-            // Log the error
+            message.reply('Herkes Arzularken Ben ** Geçerim :middle_finger: ');
             console.error(err);
           });
       } else {
-        // The mentioned user isn't in this guild
         message.reply("ERROR!");
       }
-      // Otherwise, if no user was mentioned
     } else {
       message.reply("ERROR!");
     }
